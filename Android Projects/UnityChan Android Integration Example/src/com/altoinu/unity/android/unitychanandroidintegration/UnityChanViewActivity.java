38d84563b702d7c8613589a770163aeda8b86fda
Kaoru Kawashima <altoinu@users.noreply.github.com>
2015-03-06 18:38:09 -0500

    Added some notes

--- Android Projects/UnityChan Android Integration Example/src/com/altoinu/unity/android/unitychanandroidintegration/UnityChanViewActivity.java
+++ Android Projects/UnityChan Android Integration Example/src/com/altoinu/unity/android/unitychanandroidintegration/UnityChanViewActivity.java
@@ -1,3 +1,10 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * Copyright (c) 2015 Kaoru Kawashima @altoinu http://altoinu.com
+ */
 package com.altoinu.unity.android.unitychanandroidintegration;
 
 import android.content.Intent;
--- Unity Projects/UnityChan/Assets/Scripts/Splash Screen/LicenseText.cs
+++ Unity Projects/UnityChan/Assets/Scripts/Splash Screen/LicenseText.cs
@@ -1,4 +1,11 @@
-﻿using UnityEngine;
+﻿/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * Copyright (c) 2015 Kaoru Kawashima @altoinu http://altoinu.com
+ */
+using UnityEngine;
 using System.Collections;
 
 public class LicenseText : MonoBehaviour {
